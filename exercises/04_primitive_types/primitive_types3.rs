f8c85acf3291fdf02e07631c01490c00e8ed6e07
Pares <alfariz2004@gmail.com>
2025-08-02 21:30:24 +0700

    feat: done today lesson

---
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // TODO: Create an array called `a` with at least 100 elements in it.
+    let a = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        2,3,1,4,5,5,3,3,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed");
+    }
+}
